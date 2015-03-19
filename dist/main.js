a138b8ec2dccccd0ad88d84cd94a479b7a82ba3a
Mateo Gianolio <gianoliomateo@gmail.com>
2015-03-19 17:01:21 +0100

    add desc | solve issues

--- dist/main.js
+++ dist/main.js
@@ -4,9 +4,11 @@ $(function() {
       height = $('.container').height();
 
   var source = $('.source');
+  
+  console.log(source.width());
   source.css({
-    left: (width / 2 - (source.width() / 2)),
-    top: (height / 2 - (source.height() / 2))
+    left: (width / 2) - (source.width() / 2),
+    top: (height / 2) - (source.height() / 2)
   });
 
   source.keypress(keypress);
@@ -25,31 +27,43 @@ $(function() {
     
     var source = $('.source'),
         element,
-        glossary;
+        word,
+        glossary,
+        angle;
     
     source.addClass('rotate');
     $.get(url + query, function(nodes) {
       setTimeout(function() { source.removeClass('rotate') }, 300);
       nodes.forEach(function(node, i) {
-        element = document.createElement('span');
+        element = document.createElement('div');
         
+        word = node.word.split('_').join(' ');
         glossary = node.glossary
           .split(';') // text after ';' is example(s)
           .shift(); // skip example (for now)
+        angle = Math.atan(node.position.y / node.position.x);
         
         $(element)
           .css({
             position: 'absolute',
             opacity: 0,
-            transform: 'rotate(' + Math.atan(node.position.y / node.position.x) + 'rad)'
+            transform: 'rotate(' + angle + 'rad)'
           })
           .addClass('node')
           .addClass(node.type)
           .addClass('' + i)
-          .html(node.word.split('_').join(' '))
-          .attr('data-title', glossary);
+          .html(word)
+          .attr('data-glossary', glossary)
+          .attr('data-word', word)
+          .click(function() {
+            var query = $(this).attr('data-word');
+            source.val(query);
+            search(query);
+          });
 
         $('.container').append(element);
+        
+        
 
         // grab element again when it has been given width
         element = $('.' + i);
@@ -58,9 +72,35 @@ $(function() {
           x: (width / 2 - element.width() / 2),
           y: (height / 2 - element.height() / 2)
         }, target = {
-          x: !(i % 2) ? ((width / 1.3) * node.position.x) : (width * node.position.x),
-          y: !(i % 2) ? ((height / 1.3) * node.position.y) : (height * node.position.y)
+          x: !(i % 2) ? ((width / 1.4) * node.position.x) : ((width / 1.2) * node.position.x),
+          y: !(i % 2) ? ((height / 1.4) * node.position.y) : ((height / 1.2) * node.position.y)
         };
+        
+        var leaf,
+            increment,
+            j, y;
+        
+        // remove first synonym
+        node.children.shift();
+        
+        for(j = 0; j < node.children.length; j++) {
+          child = node.children[j];
+          leaf = document.createElement('span');
+          
+          y = 48 + 20 * j;
+          
+          $(leaf)
+            .css({
+              position: 'absolute',
+              left: 0,
+              top: y,
+            })
+            .addClass('synonym')
+            .addClass(node.type)
+            .html(child.word.split('_').join(' '));
+          
+          $(element).append(leaf);
+        }
 
         element
           .css({
